3560144de09346b8b83d9d14dff037aedd8f894c
Aki Inoyatov <info@gravityelements.com>
2024-07-23 19:27:27 -0700

    pt-4 fixed

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between pt-4">
+    <main className="flex min-h-screen flex-col items-center justify-between lg:pt-4">
       <div className="z-10 w-full px-4 items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed lg:mb-4 left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Uncharted Ventures&nbsp;
