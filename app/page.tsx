26c676f562b2ef848080e6f40dca86e3ba77b835
Aki Inoyatov <info@gravityelements.com>
2024-08-19 21:37:04 -0700

    color updates

--- app/page.tsx
+++ app/page.tsx
@@ -208,7 +208,7 @@ export default async function Home() {
         </div>
       </div>
 
-      <div className="flex flex-row my-8 place-items-center bg-blue-400 rounded-xl hover:bg-blue-500 cursor-pointer transition-transform transform hover:scale-125">
+      <div className="flex flex-row my-8 place-items-center bg-blue-500 rounded-xl hover:bg-blue-700 cursor-pointer transition-transform transform hover:scale-125">
         <div className="p-1 m-2 ml-4 rounded-full bg-white">
           <Icon 
             icon="flat-color-icons:cell-phone"
@@ -216,7 +216,7 @@ export default async function Home() {
             height={20}
           />
         </div>
-        <button className="flex my-4 text-[16px] mr-4 text-white font-semibold">Call Me Today: 602-566-3816</button>
+        <button className="flex my-4 text-[16px] mr-4 text-white font-semibold">Call Us Today</button>
       </div>
 
       <div className="flex flex-col  w-full bg-image-grid-options bg-cover bg-bottom  md:bg-no-repeat  lg:h-1/2 justify-center items-center">
@@ -315,7 +315,7 @@ export default async function Home() {
           </div>
         </div>
 
-        <div className="flex flex-row my-8 place-items-center bg-blue-400 rounded-xl hover:bg-blue-500 cursor-pointer transition-transform transform hover:scale-125">
+        <div className="flex flex-row my-8 place-items-center bg-blue-500 rounded-xl hover:bg-blue-700 cursor-pointer transition-transform transform hover:scale-125">
           <div className="p-1 m-2 ml-4 rounded-full bg-white">
             <Icon 
               icon="flat-color-icons:cell-phone"
@@ -323,17 +323,17 @@ export default async function Home() {
               height={20}
             />
           </div>
-          <button className="flex my-4 text-[16px] mr-4 text-white font-semibold">Call Me Today: 602-566-3816</button>
+          <button className="flex my-2 text-[16px] mr-4 text-white font-semibold">Call Us Today</button>
         </div>
       </div>
 
-      <div className="flex flex-col py-4 gap-2 bg-black w-full text-orange-400 items-center text-3xl dark:border-t dark:border-b">
+      <div className="flex flex-col py-4 gap-2 bg-black w-full text-[#f4b400] items-center text-3xl dark:border-t dark:border-b">
         <p>No Commission</p>
         <p>No Hidden Charges</p>
-        <button className="cursor-pointer transition-transform transform hover:scale-125 bg-orange-400 rounded-xl text-black my-2 flex flex-row items-center p-2 text-xl hover:bg-orange-500">
-          <Icon icon="noto:heavy-dollar-sign" width={24} height={24}/>
+        <button className="cursor-pointer  transition-transform  transform hover:scale-125 rounded-xl bg-blue-500 text-white my-2 flex flex-row p-2 gap-2 text-xl hover:bg-blue-700">
+          <Icon icon="fa6-solid:sack-dollar" width={24} height={24} />
           <p>Free Cash Offer</p>
-        </button>
+      </button>
       </div>
 
       <div className="flex flex-col p-2 pb-8 pt-16 space-y-4">
@@ -434,75 +434,75 @@ export default async function Home() {
       </div>
 
       <div className="flex flex-col p-4 md:flex-row md:justify-center gap-4 xl:gap-8 w-full py-8">
-        <div className="flex flex-col text-lg md:text-lg ml:text-xl text-white font-normal md:font-thin p-4 bg-blue-500 rounded-xl">
-          <div className="flex flex-row space-x-4 py-4 items-center border-b-2 border-b-green-500">
-            <div className="flex items-center justify-center h-9 w-9 rounded-full bg-green-500">
-              <Icon icon="game-icons:check-mark" color="white"/>
+        <div className="flex flex-col text-lg md:text-lg ml:text-xl  md:font-thin p-4">
+          <div className="flex flex-row space-x-4 py-4 items-center border-b-2">
+            <div className="flex items-center justify-center">
+              <Icon icon="game-icons:check-mark" color="black"/>
             </div>
             <p>We buy properties in any condition</p>
           </div>
-          <div className="flex flex-row space-x-4 py-4 items-center border-b-2 border-b-green-500">
-            <div className="flex items-center justify-center h-9 w-9 rounded-full bg-green-500">
-              <Icon icon="game-icons:check-mark" color="white"/>
+          <div className="flex flex-row space-x-4 py-4 items-center border-b-2">
+            <div className="flex items-center justify-center  ">
+              <Icon icon="game-icons:check-mark" color="black"/>
             </div>
             <p>Get an offer within 72 hours</p>
           </div>
-          <div className="flex flex-row space-x-4 py-4 items-center border-b-2 border-b-green-500">
-            <div className="flex items-center justify-center h-9 w-9 rounded-full bg-green-500">
-              <Icon icon="game-icons:check-mark" color="white"/>
+          <div className="flex flex-row space-x-4 py-4 items-center border-b-2">
+            <div className="flex items-center justify-center">
+              <Icon icon="game-icons:check-mark" color="black"/>
             </div>
             <p>Quick and easy sale</p>
           </div>
-          <div className="flex flex-row space-x-4 py-4 items-center border-b-2 border-b-green-500">
-            <div className="flex items-center justify-center h-9 w-9 rounded-full bg-green-500">
-              <Icon icon="game-icons:check-mark" color="white"/>
+          <div className="flex flex-row space-x-4 py-4 items-center border-b-2">
+            <div className="flex items-center justify-center">
+              <Icon icon="game-icons:check-mark" color="black"/>
             </div>
             <p>Personalised service</p>
           </div>
           <div className="flex flex-row space-x-4 py-2 items-center">
-            <div className="flex items-center justify-center h-9 w-9 rounded-full bg-green-500">
-              <Icon icon="game-icons:check-mark" color="white"/>
+            <div className="flex items-center justify-center">
+              <Icon icon="game-icons:check-mark" color="black"/>
             </div>
             <p>Receive a free home evaluation</p>
           </div>
         </div>
 
-        <div className="flex flex-col text-lg md:text-lg ml:text-xl text-white font-normal md:font-thin p-4 bg-blue-500 rounded-xl">
-          <div className="flex flex-row space-x-4 py-4 items-center border-b-2 border-b-red-500">
-            <div className="flex items-center justify-center h-9 w-9 rounded-full bg-red-500">
-              <Icon icon="dashicons:no-alt" color="white"/>
+        <div className="flex flex-col text-lg md:text-lg ml:text-xl md:font-thin p-4 ">
+          <div className="flex flex-row space-x-4 py-4 items-center border-b-2">
+            <div className="flex items-center justify-center">
+              <Icon icon="dashicons:no-alt" color="black"/>
             </div>
             <p>No repairs or cleaning required</p>
           </div>
-          <div className="flex flex-row space-x-4 py-4 items-center border-b-2 border-b-red-500">
-            <div className="flex items-center justify-center h-9 w-9 rounded-full bg-red-500">
-              <Icon icon="dashicons:no-alt" color="white"/>
+          <div className="flex flex-row space-x-4 py-4 items-center border-b-2">
+            <div className="flex items-center justify-center">
+              <Icon icon="dashicons:no-alt" color="black"/>
             </div>
             <p>No obligations</p>
           </div>
-          <div className="flex flex-row space-x-4 py-4 items-center border-b-2 border-b-red-500">
-            <div className="flex items-center justify-center h-9 w-9 rounded-full bg-red-500">
-              <Icon icon="dashicons:no-alt" color="white"/>
+          <div className="flex flex-row space-x-4 py-4 items-center border-b-2">
+            <div className="flex items-center justify-center">
+              <Icon icon="dashicons:no-alt" color="black"/>
             </div>
             <p>No listings or for sale boards</p>
           </div>
-          <div className="flex flex-row space-x-4 py-4 items-center border-b-2 border-b-red-500">
-            <div className="flex items-center justify-center h-9 w-9 rounded-full bg-red-500">
-              <Icon icon="dashicons:no-alt" color="white"/>
+          <div className="flex flex-row space-x-4 py-4 items-center border-b-2">
+            <div className="flex items-center justify-center">
+              <Icon icon="dashicons:no-alt" color="black"/>
             </div>
             <p>No waiting months for a buyer</p>
           </div>
-          <div className="flex flex-row space-x-4 py-4 items-center ">
-            <div className="flex items-center justify-center h-9 w-9 rounded-full bg-red-500">
-              <Icon icon="dashicons:no-alt" color="white"/>
+          <div className="flex flex-row space-x-4 py-4 items-center">
+            <div className="flex items-center justify-center">
+              <Icon icon="dashicons:no-alt" color="black"/>
             </div>
             <p>No fees or commissions charged</p>
           </div>
         </div>
       </div>
 
-      <button className="cursor-pointer  transition-transform  transform hover:scale-125 rounded-xl bg-orange-400 text-black my-2 flex flex-row items-center p-2 text-xl hover:bg-orange-500">
-          <Icon icon="noto:heavy-dollar-sign" width={24} height={24}/>
+      <button className="cursor-pointer  transition-transform  transform hover:scale-125 rounded-xl bg-blue-500 text-white my-2 flex flex-row p-2 gap-2 text-xl hover:bg-blue-700">
+          <Icon icon="fa6-solid:sack-dollar" width={24} height={24} />
           <p>Free Cash Offer</p>
       </button>
 
--- components/calendly/calendlyAppointments.jsx
+++ components/calendly/calendlyAppointments.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { InlineWidget } from 'react-calendly'
+import { InlineWidget } from 'react-calendly';
 
 const CalendlyBooking = () => {
 
